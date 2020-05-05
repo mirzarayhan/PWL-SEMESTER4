242639be09ae3358e31c0edb4d9fc4ff6eb31131
Mirza Rayhan <56281890+mirzarayhan@users.noreply.github.com>
2020-05-05 16:30:25 +0700

    Laravel Jobsheet 14

---
+++ Laravel Jobsheet 14/laravel-restapi/app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Mahasiswa;
+
+class MahasiswaController extends Controller
+{
+    public function index()
+    {
+        $data = Mahasiswa::all();
+
+        if (count($data) > 0) {
+            $res['message'] = "Success!";
+            $res['values'] = $data;
+            return response($res);
+        } else {
+            $res['message'] = "Failed!";
+            return response($res);
+        }
+    }
+
+    public function getId($id)
+    {
+        $data = Mahasiswa::where('id', $id)->get();
+
+        if (count($data) > 0) {
+            $res['message'] = "Success!";
+            $res['values'] = $data;
+            return response($res);
+        } else {
+            $res['message'] = "Failed!";
+            return response($res);
+        }
+    }
+
+    public function create(Request $request)
+    {
+        $mhs = new Mahasiswa();
+        $mhs->nama = $request->nama;
+        $mhs->nim = $request->nim;
+        $mhs->email = $request->email;
+        $mhs->jurusan = $request->jurusan;
+
+        if ($mhs->save()) {
+            $res['message'] = "Data Tersimpan!";
+            $res['values'] = $mhs;
+            return response($res);
+        } else {
+            $res['message'] = "Failed!";
+            return response($res);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $nama = $request->nama;
+        $nim = $request->nim;
+        $email = $request->email;
+        $jurusan = $request->jurusan;
+
+        $mhs = Mahasiswa::find($id);
+        $mhs->nama = $nama;
+        $mhs->nim = $nim;
+        $mhs->email = $email;
+        $mhs->jurusan = $jurusan;
+
+        if ($mhs->save()) {
+            $res['message'] = "Data Diubah!";
+            $res['values'] = "$mhs";
+            return response($res);
+        } else {
+            $res['message'] = "Failed!";
+            return response($res);
+        }
+    }
+
+    public function delete($id)
+    {
+        $mhs = Mahasiswa::where('id', $id);
+
+        if ($mhs->delete()) {
+            $res['message'] = "Deleted!";
+            return response($res);
+        } else {
+            $res['message'] = "Failed!";
+            return response($res);
+        }
+    }
+}
